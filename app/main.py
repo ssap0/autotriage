b1a320fcf9a7f41e8622b8908df561ed205fb147
ssap0 <70007733+ssap0@users.noreply.github.com>
2025-07-08 17:43:50 +0800

    testing post message sending

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.api import api_router
+import requests
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -22,3 +23,20 @@ async def root() -> dict[str, str]:
 async def health_check() -> dict[str, str]:
     """Health check endpoint for monitoring."""
     return {"status": "healthy"}
+
+posturl = 'https://candidate-ds-endpoint.onrender.com/get-category'
+
+postdata = {
+   "email_content": "a new email that is not spam!",
+   "username": "eexuanen",
+   "input_categories": ["SPAM", "IMPORTANT", "MARKETING"]
+}
+
+# Function to do a post message to to the url
+def SendPost(data):
+    response = requests.post(posturl, json = data)
+    print(response.status_code)
+    print(response.text)
+
+
+SendPost(postdata)
